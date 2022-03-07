467623d672a6ad7b46aa68eb567a16b60cd61192
Robert Susewell <susewell@live.unc.edu>
2022-03-07 17:57:40 -0500

    require

--- flips.js
+++ flips.js
@@ -1,6 +1,9 @@
 
 import { coinFlips, countFlips,} from "./modules/coin.mjs" 
 import minimist from 'minimist';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 const args = require('minimist')
 const number = args.number
