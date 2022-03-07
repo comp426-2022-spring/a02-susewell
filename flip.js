58463df12abbe3c007638cb666cbe6f493783e3d
Robert Susewell <susewell@live.unc.edu>
2022-03-07 16:39:05 -0500

    Coin flip

--- flip.js
+++ flip.js
@@ -1,14 +1,8 @@
 // Import the coinFlip function from your coin.mjs file
 
 
-import { coinFlip, coinFlips, countFlips, flipACoin } from "./modules/coin.mjs" 
+import { coinFlip } from "./modules/coin.mjs" 
 
 // Call the coinFlip function and put the return into STDOUT
 
 console.log(coinFlip());
-
-console.log(coinFlips(7));
-
-console.log(countFlips(['heads', 'heads','heads', 'tails','heads', 'tails','tails', 'heads','tails', 'heads']));
-
-console.log(flipACoin('tails'))
