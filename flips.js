237c1ffa8e59b1f491a9ce951aa19259fb258625
Robert Susewell <susewell@live.unc.edu>
2022-03-29 16:12:51 -0400

    flips

--- flips.js
+++ flips.js
@@ -1,19 +1,18 @@
 
-import { coinFlips, countFlips,} from "./modules/coin.mjs" 
-import minimist from 'minimist';
-import { createRequire } from 'module';
+import { coinFlips, countFlips} from "./modules/coin.mjs" 
+import min from 'minimist';
 
-const require = createRequire(import.meta.url);
 
-const args = require('minimist')(process.argv.slice(2))
 
-args['number']
+const args = min(process.argv.slice(2))
+
+
 
 
 var flip = [];
 
-if (args.number != null ){
-    flip = coinFlips(args.number);
+if (args['number']){
+    flip = coinFlips(args['number']);
     console.log(flip);
     console.log(countFlips(flip))
 } else {
