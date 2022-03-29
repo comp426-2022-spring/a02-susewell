3ca5781ece8a6073ad72f5d29edcad0ddb3bc3a9
Robert Susewell <susewell@live.unc.edu>
2022-03-29 16:32:11 -0400

    try this again

--- guess-flip.js
+++ guess-flip.js
@@ -5,7 +5,7 @@ const args = min(process.argv.slice(2))
 
 if (args['call'] == 'tails'){
     console.log(flipACoin('tails'));
-} else if (args['call'] == 'headss') {
+} else if (args['call'] == 'heads') {
     console.log(flipACoin('heads'));
 } else {
     console.log("Error: no input.\nUsage: node guess-flip --call=[heads|tails]")
